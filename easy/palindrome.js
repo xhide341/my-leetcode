030075c1afd105ffd319c2033f52d8916df0c1da
xhide341 <shawnehgn10@gmail.com>
2025-04-29 22:51:38 +0800

    feat: add palindrome

---
+++ easy/palindrome.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  let str = x.toString();
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
