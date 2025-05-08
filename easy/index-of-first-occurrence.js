70ad9d2071c19a6fcf8520606706db9b9c544b2e
xhide341 <shawnehgn10@gmail.com>
2025-05-08 23:10:51 +0800

    feat: add index of first occurrence

---
+++ easy/index-of-first-occurrence.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  for (let i = 0; i < haystack.length - needle.length; i++) {
+    let match = true;
+    for (let j = 0; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        match = false; // if there's a false match in this loop, break immediately
+        break;
+      }
+    }
+    if (match) return i;
+  }
+  return -1;
+};
